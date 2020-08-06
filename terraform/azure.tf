03d98476965835a90fcc47f582c8cc464fd0584a
sainohira-s <xsainohira.shota@contract.isid.co.jp>
2020-08-06 14:52:11 +0900

    test

---
+++ terraform/azure.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  version = "2.13.0"
+  features {}
+}
+
+terraform {
+  required_version = ">= 0.12.6"
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state"
+    storage_account_name = "terraformcistate"
+    container_name       = "tfstate"
+    key                  = "prodsai20200806.terraform.tfstate"
+  }
+}
